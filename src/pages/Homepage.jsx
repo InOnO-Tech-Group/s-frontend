7c5880c3f7e4f0c51d0c5a339d4a0e3459684475
Ireme-Nyawe <akimana.inono@gmail.com>
2024-12-09 09:22:12 +0200

    create footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import SEO from '../components/re-usable/SEO';
 import Header from '../components/clients/Header';
+import Footer from '../components/clients/Footer';
 
 
 const Homepage = () => {
@@ -8,6 +9,7 @@ const Homepage = () => {
     <>
       <SEO title="ES Gishom Home" description="The homepage of ES Gishoma" />
       <Header />
+      <Footer />
     </>
   );
 };
